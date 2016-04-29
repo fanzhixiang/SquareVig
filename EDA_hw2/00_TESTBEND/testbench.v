29bebe608bbc18665b37b490a244f7b3fe790381
timerg <ggteven@gmail.com>
2016-04-29 18:35:02 +0800

    ..

--- EDA_hw2/00_TESTBEND/testbench.v
+++ EDA_hw2/00_TESTBEND/testbench.v
@@ -1,5 +1,6 @@
-`timescale 10ns/1ps
-`define CLK 10
+`timescale 1ns/1ps
+
+`define CLK 50
 
 module testbench;
 
@@ -10,16 +11,19 @@ wire [31:0]sqrt;
 square_root_finder square_root_finder1(.clk(clk), .rst(rst), .in(in), .sqrt(sqrt));
 
 always begin #(`CLK/2) clk = ~ clk; end
-    initial begin
+
+initial begin
     clk = 1'b0;
-    rst = 1'b0;
+    rst = 1'b1;
     in =  16'd0;
-    #10 in = 16'd3; rst = 1'b1;
-    #10 rst = 1'b0;
+    #170 in = 16'd3; rst = 1'b0;
+    #100 rst = 1'b1;
+    #5000 $finish;
+end
 
 
 
-
+ initial begin
     `ifdef RTL
 	    $fsdbDumpfile("square_root_finder.fsdb");
 		$fsdbDumpvars;
@@ -29,6 +33,6 @@ always begin #(`CLK/2) clk = ~ clk; end
         $fsdbDumpfile("square_root_finder_syn.fsdb");
         $fsdbDumpvars;
 	    `endif
-    # 1000 $finish;
+    #5000 $finish;
   end
 endmodule
--- EDA_hw2/01_RTL/square_root_finder.v
+++ EDA_hw2/01_RTL/square_root_finder.v
@@ -1,129 +1,136 @@
 module square_root_finder(
     input wire rst,
     input wire clk,
-    input wire [15 : 0]in,
-    output reg [31 : 0] sqrt
+    input wire [15 : 0] in,
+    output wire [31 : 0] sqrt
     );
 reg [1 : 0] state, state_next;
 reg [0 : 0] cat, cat_temp;
 reg [31 : 0] up, up_temp, low, low_temp, g, g_temp;
-reg[31 : 0] inx;
+wire [31 : 0] inx;
 
-
-// always@(posedge clk)        //closeEdge
-//     if (inx > (g * g)) begin
-//         if((inx - g * g) < 32'd02) sqrt = g;
-//         else begin
-//             up = up;
-//             low = g;
-//             g = ((up + low) >> 1);
-//             sqrt = g;
-//         end
-//     else begin
-//         if ((g * g - inx) < 32'd02) sqrt = g;
-//         else begin
-//             up = g;
-//             low = low;
-//             g = ((up + low) >> 1);
-//             sqrt = g;
-//     end
-// end
-
-// always@(state)        //closeEdge
-//     case (inx > (g * g))
-//         1'b1: begin
-//             case((inx - g * g) < 32'd02)
-//                 1'b1: state = 2'b11;
-//                 1'b0: begin
-//                     // g = g_temp;
-//                     up = up;
-//                     low = g;
-//                     g_temp = ((up + g) >> 1);
-//                     state = 2'b01;
-//                 end
-//             endcase
-//         end
-//         1'b0: begin
-//             case((g * g - inx) < 32'd02)
-//                 1'b1: state = 2'b11;
-//                 1'b0: begin
-//                     // g = g_temp;
-//                     up = g;
-//                     low = low;
-//                     g_temp = ((g + low) >> 1);
-//                     sqrt = g;
-//                     state = 2'b01;
-//                 end
-//             endcase
-//         end
-//     endcase
-
-// always @ (negedge clk) begin
-//     g <= g_temp;
-//     // sqrt <= g_temp;
-// end
+assign sqrt = g;
 
 always@( * )
     case(state)
-        2'b11: sqrt = g;
-        2'b00: begin sqrt = g; state_next = 2'b01;
-                up_temp = (in << 6);
+        2'b11: begin
+                state_next = state;
+                cat_temp = cat;
+                up_temp = up;
+                low_temp = low;
+                g_temp = g;
+            end
+        2'b00: begin
+                state_next = 2'b01;
+                cat_temp = 1'b0;
+                up_temp = {10'd0, in, 6'd0};
                 low_temp = 32'd0;
                 g_temp = 32'd0;
-                cat_temp = 1'd0;
             end
         2'b01: begin
             if (inx > (g * g))
                 begin
-                    if ((inx - g * g) < 32'd02) state_next = 2'b11;
+                    if ((inx - g * g) < 32'd02) begin
+                        state_next = 2'b11;
+                        cat_temp = cat;
+                        up_temp = up;
+                        low_temp = low;
+                        g_temp = g;
+
+                    end
                     else begin
                         cat_temp = 1'b1;
                         state_next = 2'b10;
+                        up_temp = up;
+                        low_temp = low;
+                        g_temp = g;
                     end
                 end
             else
                 begin
-                    if ((g * g - inx) < 32'd02) state_next = 2'b11;
+                    if ((g * g - inx) < 32'd02) begin
+                        state_next = 2'b11;
+                        cat_temp = cat;
+                        up_temp = up;
+                        low_temp = low;
+                        g_temp = g;
+                    end
                     else begin
                         cat_temp = 1'b0;
                         state_next = 2'b10;
+                        up_temp = up;
+                        low_temp = low;
+                        g_temp = g;
                     end
                 end
             end
         2'b10: begin
             case(cat)
                 1'b1: begin
+                    cat_temp = cat;
+                    up_temp = up;
                     low_temp = g;
                     g_temp = ((up + g) >> 1);
                     state_next = 2'b01;
-                    sqrt = g;
                 end
                 1'b0: begin
+                    cat_temp = cat;
                     up_temp = g;
+                    low_temp = low;
                     g_temp = ((g + low) >> 1);
                     state_next = 2'b01;
-                    sqrt = g;
                 end
             endcase
          end
     endcase
 
-always @ (posedge clk or posedge rst) begin
-if(rst)begin
-    state <= state_next;
-    inx <= in << 12;
-    up <= up_temp;
-    low <= low_temp;
-    cat <= cat_temp;
-    g <= g_temp;
+
+
+
+always @ (negedge rst or  posedge clk) begin
+if(~rst)begin
+    state <= 2'b00;
     end
 else begin
-    state <= 2'b00;
-    inx <= in << 12;
-    up <= (in << 6);
+    state <= state_next;
+    end
+end
+
+assign inx = {4'd0, in, 12'd0};
+
+always @ (negedge rst or  posedge clk) begin
+if(~rst)begin
+    up <= {10'd0, in, 6'd0};
+    end
+else begin
+    up <= up_temp;
+    end
+end
+
+always @ (negedge rst or  posedge clk) begin
+if(~rst)begin
     low <= 32'd00;
+    end
+else begin
+    low <= low_temp;
+    end
+end
+
+always @ (negedge rst or  posedge clk) begin
+if(~rst)begin
     cat <= 1'b0;
+    end
+else begin
+    cat <= cat_temp;
+    end
+end
+
+always @ (negedge rst or  posedge clk) begin
+if(~rst)begin
     g <= 32'd00;
+    end
+else begin
+    g <= g_temp;
     end
 end
 
